92c11ecc532b5b988c98bdecbbab3b6cd3789f88
Sweidan Omár <omcsesz@gmail.com>
2017-03-01 13:49:54 +0100

    Add files via upload

---
+++ 23 este/Whatever/Whatever/display.h
@@ -0,0 +1,20 @@
+/*
+ * display.h
+ *
+ * Created: 2016. 05. 16. 20:12:24
+ *  Author: semuser
+ */ 
+
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+#include <avr/io.h>
+
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+void lcd_putint(uint16_t input);
+
+
+#endif /* DISPLAY_H_ */
---
+++ 23 este/Whatever/Whatever/writeToLCD.s
@@ -0,0 +1,7 @@
+
+/*
+ * writeToLCD.s
+ *
+ * Created: 2016.05.22. 11:59:15
+ *  Author: Omcsesz
+ */ 
