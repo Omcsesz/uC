e505e162bb53b556004090a660693c562829096d
Sweidan Omár <omcsesz@gmail.com>
2017-03-01 13:48:34 +0100

    Add files via upload

---
+++ 23 este/Whatever/Whatever/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+display.c
+
+LCD.c
+
+UART.c
+
+Whatever.c
+
+writeToLCD.s
+
---
+++ 23 este/Whatever/Whatever/Debug/writeToLCD.d
@@ -0,0 +1,1 @@
+writeToLCD.d writeToLCD.o: .././writeToLCD.s
